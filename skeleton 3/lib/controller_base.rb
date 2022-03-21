91c03723fddd197527f61626949c8b0c96626058
App Academy Student <appacademystudent@Apps-Mac-mini-2.local>
2022-03-21 17:08:14 -0400

    02

--- skeleton 3/lib/controller_base.rb
+++ skeleton 3/lib/controller_base.rb
@@ -8,33 +8,44 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    req = Rack::Request.new
+    res = Rack::Response.new
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = 302
+    @res['Location'] = url
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    nil
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    
   end
 
   # method exposing a `Session` object
   def session
+
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+
   end
 end
 
